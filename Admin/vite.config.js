f633ab30a692c9017b73f0b30680f3aabfd95272
Dhafin06 <aksanidradhafin05@gmail.com>
2025-05-27 14:09:24 +0700

    push composer

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -11,6 +11,6 @@ export default defineConfig({
         tailwindcss(),
     ],
 });
-import tailwindcss from '@tailwindcss/vite';
+
 
 
